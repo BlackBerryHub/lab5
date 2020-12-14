d2565ebae0c3d37537a7930ff3ce04f5783cf305
Black Berry <marat4800@gmail.com>
2020-12-14 17:59:39 +0200

    Добавьте файлы проекта.

---
+++ DataBaseController.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class DataBaseController
+    {
+        private SqlConnection connect = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connect.Close();
+        }
+
+        public void InsertDocuments(int IdEquipment, int IdProcedure)
+        {
+            string sql = string.Format("INSERT INTO Documents VALUES(@ID_Equipment, @ID_Procedure)");
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.Parameters.AddWithValue("@ID_Equipment", IdEquipment);
+                cmd.Parameters.AddWithValue("@ID_Procedure", IdProcedure);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertNaprav(int IdPacients, int IdProcedure)
+        {
+            string sql = string.Format("INSERT INTO Naprav VALUES(@ID_Pacients, @ID_Procedure)");
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.Parameters.AddWithValue("@ID_Pacients", IdPacients);
+                cmd.Parameters.AddWithValue("@ID_Procedure", IdProcedure);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertEquipments(string nameEquipments, string Appointments)
+        {
+            string sql = string.Format("INSERT INTO Equipments VALUES(@nameEquipments, @Appointments)");
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.Parameters.AddWithValue("@nameEquipments", nameEquipments);
+                cmd.Parameters.AddWithValue("@Appointments", Appointments);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void InsertPacients(string Name, string LastName)
+        {
+            string sql = string.Format("INSERT INTO Pacients VALUES(@Name, @LastName)");
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.Parameters.AddWithValue("@Name", Name);
+                cmd.Parameters.AddWithValue("@LastName", LastName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void InsertProcedure(string Name, int Cost)
+        {
+            string sql = string.Format("INSERT INTO Procedure VALUES(@Name, @Cost)");
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.Parameters.AddWithValue("@Name", Name);
+                cmd.Parameters.AddWithValue("@Cost", Cost);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable GetDataDocuments()
+        {
+            DataTable doc = new DataTable();
+            string sql = "SELECT * FROM Documents";
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                doc.Load(dr);
+                dr.Close();
+            }
+            return doc;
+        }
+        public DataTable GetDataNaprav()
+        {
+            DataTable nap = new DataTable();
+            string sql = "SELECT * FROM Naprav";
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                nap.Load(dr);
+                dr.Close();
+            }
+            return nap;
+        }
+        public DataTable GetDataEquipments()
+        {
+            DataTable equip = new DataTable();
+            string sql = "SELECT * FROM Equipments";
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                equip.Load(dr);
+                dr.Close();
+            }
+            return equip;
+        }
+        public DataTable GetDataPacients()
+        {
+            DataTable pacients = new DataTable();
+            string sql = "SELECT * FROM Pacients";
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                pacients.Load(dr);
+                dr.Close();
+            }
+            return pacients;
+        }
+        public DataTable GetDataProcedure()
+        {
+            DataTable proc = new DataTable();
+            string sql = "SELECT * FROM \"Procedure\"";
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                proc.Load(dr);
+                dr.Close();
+            }
+            return proc;
+        }
+
+        public void DeleteDocuments(int ID_Documents)
+        {
+            string sql = string.Format("DELETE FROM Documents WHERE ID_Documents = '{0}'", ID_Documents);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Запис не існує", ex);
+                    throw error;
+                }
+            }
+        }
+        public void DeleteEquipments(int ID_Equipment)
+        {
+            string sql = string.Format("DELETE FROM Equipments WHERE ID_Equipment = '{0}'", ID_Equipment);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Запис не існує", ex);
+                    throw error;
+                }
+            }
+        }
+        public void DeleteNaprav(int ID_Naprav)
+        {
+            string sql = string.Format("DELETE FROM Naprav WHERE ID_Naprav = '{0}'", ID_Naprav);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Запис не існує", ex);
+                    throw error;
+                }
+            }
+        }
+        public void DeletePacients(int ID_Pacients)
+        {
+            string sql = string.Format("DELETE FROM Pacients WHERE ID_Pacients = '{0}'", ID_Pacients);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Запис не існує", ex);
+                    throw error;
+                }
+            }
+        }
+        public void DeleteProcedure(int ID_Procedure)
+        {
+            string sql = string.Format("DELETE FROM Procedure WHERE ID_Procedure = '{0}'", ID_Procedure);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Запис не існує", ex);
+                    throw error;
+                }
+            }
+        }
+        public void UpdateDocuments(int ID_Documents, int ID_Equipment, int ID_Procedure)
+        {
+            string sql = string.Format("UPDATE Documents SET ID_Equipment = '{0}', ID_Procedure = '{1}' WHERE ID_Documents = '{2}'", 
+                ID_Equipment, ID_Procedure, ID_Documents);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void UpdateEquipments(int ID_Equipment, string nameEquipment, string Appointments)
+        {
+            string sql = string.Format("UPDATE Equipments SET nameEquipment = '{0}', Appointments = '{1}' WHERE ID_Equipment = '{2}'",
+                nameEquipment, Appointments, ID_Equipment);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void UpdateNaprav(int ID_Naprav, int ID_Pacients, int ID_Procedure)
+        {
+            string sql = string.Format("UPDATE Naprav SET ID_Pacients = '{0}', ID_Procedure = '{1}' WHERE ID_Naprav = '{2}'",
+                ID_Pacients, ID_Procedure, ID_Naprav);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void UpdatePacients(int ID_Pacients, string Name, string LastName)
+        {
+            string sql = string.Format("UPDATE Pacients SET Name = '{0}', LastName = '{1}' WHERE ID_Pacinents = '{2}'",
+                Name, LastName, ID_Pacients);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void UpdateProcedure(int ID_Procedure, string Name, int Cost)
+        {
+            string sql = string.Format("UPDATE Procedure SET Name = '{0}', Cost = '{1}' WHERE ID_Procedure = '{2}'",
+                Name, Cost, ID_Procedure);
+            using (SqlCommand cmd = new SqlCommand(sql, this.connect))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["ClinicConnection"].ConnectionString;
+        SqlDataAdapter adapter;
+        DataSet dataSet = new DataSet("Clinic");
+        SqlCommandBuilder sqlCommandBuilder;
+        int checkTable = 0;
+
+        DataBaseController dbc = new DataBaseController();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbc.OpenConnection(connectionString);
+            //string connectionString = @"Data Source=.\blackberry;Initial Catalog=Clinic;Integrated Security=False";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbc.GetDataDocuments();
+            checkTable = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbc.GetDataNaprav();
+            checkTable = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbc.GetDataEquipments();
+            checkTable = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbc.GetDataPacients();
+            checkTable = 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dbc.GetDataProcedure();
+            checkTable = 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            switch (checkTable)
+            {
+                case 1: 
+                    {
+                        dbc.InsertDocuments(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataDocuments();
+                        break;
+                    };
+                case 2: 
+                    {
+                        dbc.InsertNaprav(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataNaprav();
+                        break; 
+                    };
+                case 3: 
+                    { 
+                        dbc.InsertEquipments(textBox1.Text,textBox2.Text);
+                        dataGridView1.DataSource = dbc.GetDataEquipments();
+                        break; 
+                    };
+                case 4: 
+                    {
+                        dbc.InsertPacients(textBox1.Text, textBox2.Text);
+                        dataGridView1.DataSource = dbc.GetDataPacients();
+                        break; 
+                    };
+                case 5: 
+                    {
+                        dbc.InsertProcedure(textBox1.Text, Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataProcedure();
+                        break; 
+                    };
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            switch (checkTable)
+            {
+                case 1: 
+                    {
+                        dbc.DeleteDocuments(Convert.ToInt32(textBox3.Text));
+                        dataGridView1.DataSource = dbc.GetDataDocuments();
+                        break; 
+                    };
+                case 2: 
+                    {
+                        dbc.DeleteNaprav(Convert.ToInt32(textBox3.Text));
+                        dataGridView1.DataSource = dbc.GetDataNaprav();
+                        break; 
+                    };
+                case 3: 
+                    {
+                        dbc.DeleteEquipments(Convert.ToInt32(textBox3.Text));
+                        dataGridView1.DataSource = dbc.GetDataEquipments();
+                        break; 
+                    };
+                case 4: 
+                    {
+                        dbc.DeletePacients(Convert.ToInt32(textBox3.Text));
+                        dataGridView1.DataSource = dbc.GetDataPacients();
+                        break; 
+                    };
+                case 5:
+                    {
+                        dbc.DeleteProcedure(Convert.ToInt32(textBox3.Text));
+                        dataGridView1.DataSource = dbc.GetDataProcedure();
+                        break; 
+                    };
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            switch (checkTable)
+            { 
+                case 1: 
+                    {
+                        dbc.UpdateDocuments(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataDocuments();
+                        break; 
+                    };
+                case 2: 
+                    {
+                        dbc.UpdateNaprav(Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataNaprav();
+                        break; 
+                    };
+                case 3: 
+                    {
+                        dbc.UpdateEquipments(Convert.ToInt32(textBox3.Text), textBox1.Text, textBox2.Text);
+                        dataGridView1.DataSource = dbc.GetDataEquipments();
+                        break; 
+                    };
+                case 4: 
+                    {
+                        dbc.UpdatePacients(Convert.ToInt32(textBox3.Text),textBox1.Text,textBox2.Text);
+                        dataGridView1.DataSource = dbc.GetDataPacients();
+                        break; 
+                    };
+                case 5: 
+                    {
+                        dbc.UpdateProcedure(Convert.ToInt32(textBox3.Text), textBox1.Text, Convert.ToInt32(textBox2.Text));
+                        dataGridView1.DataSource = dbc.GetDataProcedure();
+                        break; 
+                    };
+            }
+        }
+    }
+}
